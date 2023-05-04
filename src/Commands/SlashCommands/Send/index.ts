ce9aba50ea5f7436aba4cef179cb6b02d2daa700
mark-p7 <markdeguzman1142@gmail.com>
2023-05-03 19:41:10 -0700

    Quick fix: Added defer function to interaction before firing off a response

--- src/Commands/SlashCommands/Send/index.ts
+++ src/Commands/SlashCommands/Send/index.ts
@@ -24,18 +24,15 @@ export const SendCommand: Command = {
     const channelId: string = interaction.channelId;
     const content = interaction.options.get("message")?.value;
     const channel = await client.channels.fetch(channelId);
-    if(interaction.replied) return;
     if (typeof content === "string" && channel) {
       await (channel as TextChannel).send(content);
-      await interaction.reply({
+      await interaction.editReply({
         content: "Message sent",
-        ephemeral: true,
       });
       return;
     } else {
-      await interaction.reply({
+      await interaction.editReply({
         content: "An error has occurred",
-        ephemeral: true,
       });
       return;
     }
--- src/Commands/SlashCommands/handleSlashCommand.ts
+++ src/Commands/SlashCommands/handleSlashCommand.ts
@@ -1,5 +1,6 @@
 import { Client, CommandInteraction, Interaction, Events } from "discord.js";
 import { Commands } from "./Commands";
+import { setTimeout } from "timers/promises";
 
 export const interactionHandler = (client: Client): void => {
   client.on(Events.InteractionCreate, async (interaction: Interaction) => {
@@ -15,12 +16,18 @@ export const handleSlashCommand = async (
 ): Promise<void> => {
   const slashCommand = Commands.find((c) => c.name === interaction.commandName);
   if (!slashCommand) {
-    await interaction.followUp({
+    await interaction.reply({
       content: "An error has occurred",
       ephemeral: true,
     });
     return;
   } else {
+    await interaction.deferReply({ ephemeral: true });
+    await wait(4);
     slashCommand.run(client, interaction);
   }
+};
+
+const wait = async (seconds: number) => {
+  await setTimeout(seconds * 1000);
 };
