290c97eae11978aa301993e94129374f8cebb6f8
mark-p7 <markdeguzman1142@gmail.com>
2024-01-26 18:24:51 -0800

    Added profanity filter to censor the word and then sent the message through the bot.

--- src/Commands/SlashCommands/Send/index.ts
+++ src/Commands/SlashCommands/Send/index.ts
@@ -3,10 +3,17 @@ import {
   Client,
   ApplicationCommandType,
   TextChannel,
-  TextBasedChannel,
-  ApplicationCommandStringOptionData,
+  Message,
 } from "discord.js";
 import { Command } from "@/Types/CommandTypes";
+import { Profanity, ProfanityOptions } from "@2toad/profanity";
+import { CensorType } from "@2toad/profanity/dist/models";
+
+const profanityOptions: ProfanityOptions = {
+  wholeWord: false,
+  grawlix: "\\*\\*\\*\\*\\*",
+  grawlixChar: "\\*",
+};
 
 export const SendCommand: Command = {
   name: "anon",
@@ -21,20 +28,36 @@ export const SendCommand: Command = {
   ],
   run: async (client: Client, interaction: CommandInteraction) => {
     if (!interaction.isCommand()) return;
+
     const channelId: string = interaction.channelId;
     const content = interaction.options.get("message")?.value;
     const channel = await client.channels.fetch(channelId);
+    const profanity = new Profanity(profanityOptions);
+
     if (typeof content === "string" && channel) {
-      await (channel as TextChannel).send(content);
+      // Filter word and log it
+      const wordSent = profanity.censor(content, CensorType.AllVowels);
+      console.log(wordSent);
+
+      // Send the message through the bot
+      const message: Message<true> = await (channel as TextChannel).send(
+        wordSent
+      );
+
+      // Notify the user that the message has been sent
+      const returnString =
+        `Message has been sent | ` +
+        `Content: ${message.content} | ` +
+        `Id: ${message.id} | ` +
+        `https://discord.com/channels/${message.guildId}/${message.channelId}/${message.id}`;
+
       await interaction.editReply({
-        content: "Message sent",
+        content: returnString,
       });
-      return;
     } else {
       await interaction.editReply({
         content: "An error has occurred",
       });
-      return;
     }
   },
 };
